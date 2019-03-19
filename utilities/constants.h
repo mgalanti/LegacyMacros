70a342bd44c548ea730f3b9492e58d4d7db2e41b
mgalanti <mario.galanti@cern.ch>
2019-03-19 11:04:21 +0100

    Initial commit
    Initial merge of old code

---
+++ utilities/constants.h
@@ -0,0 +1,21 @@
+#ifndef constants_h
+#define constants_h
+
+
+
+namespace constants
+{
+  // Mathematical constants
+  const double PI = 3.141592653589793;
+
+  // Particle properties
+  const double muonMass = 0.1056583715;
+  const double pionMass = 0.13957018;
+  const double kaonMass = 0.493677;
+  const double jPsiMass = 3.096916;
+  
+}
+
+
+
+#endif // constants_h
